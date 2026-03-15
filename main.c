624ebcac27378343b143a0665dd89f65d774dee6
EvDes190 <89368408+EvDes190@users.noreply.github.com>
2026-03-15 19:03:40 +0300

    little uncompleted rendering

--- main.c
+++ main.c
@@ -1,6 +1,78 @@
+#include <windows.h>
+#include <unistd.h>
+#include <time.h>
+#include <pthread.h>
+#include <conio.h>
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+#import "src/state.c"
+#import "render/window.c"
+
+#define FPS 60
+#define CLOCKS_PER_FRAME (CLOCKS_PER_SEC / FPS)
+
+
+
+
+void game_during(f_state* game_state) {
+    clock_t start = clock();
+    int move_x = 0, move_y = 0;
+    int key = 0;
+
+    while (update_state(game_state, move_x, move_y) != 1) {
+        move_x = 0, move_y = 0;
+        clock_t begin = clock();
+        clock_t until = 0;
+        while (until < CLOCKS_PER_FRAME) {
+            until = clock() - begin;
+
+            if (kbhit()) {
+                key = getch();
+                switch (key) {
+                    case 'w':
+                        move_y = 1;
+                        continue;
+                    case 's':
+                        move_y = -1;
+                        continue;
+                    case 'd':
+                        move_x = 1;
+                        continue;
+                    case 'a':
+                        move_x = -1;
+                    default: ;
+                }
+            }
+        }
+    }
+
+    game_state->time += clock() - start;
+
+}
+
+int main() {
+    f_state* game_state = malloc(sizeof(*game_state));
+    reset_state(game_state);
+    renderstate_t* renderstate = malloc(sizeof(*renderstate));
+    renderstate->height = 100, renderstate->width = 100;
+    renderstate->hwnd = GetForegroundWindow();
+    renderstate->hdc = GetDC(renderstate->hwnd);
+
+    pthread_t* thread = malloc(sizeof(*thread));
+    struct render_args r_args = {
+        .state = renderstate,
+        .field_range = FIELD_RANGE,
+        //TODO: fix warning
+        .colors = game_state->colors};
+    pthread_create(thread, NULL, render, &r_args);
+
+
+    //TODO code system of restarting game
+    while (1) {
+
+    }
+
+    free(game_state);
+    free(renderstate);
+    free(thread);
 }
--- render/window.c
+++ render/window.c
@@ -4,10 +4,34 @@
 typedef struct RenderState {
     HWND hwnd;
     HDC hdc;
-    HPEN hPen;
-    HBRUSH hBrush;
 
-    int fps;
+
     int width;
     int height;
 } renderstate_t;
+
+struct render_args {
+    renderstate_t* state;
+    int field_range;
+    unsigned int** colors;
+};
+
+// void render(renderstate_t* state, int field_range, int colors[field_range][field_range]) {
+void* render(void *args) {
+    struct render_args* r_args = args;
+
+
+    for (int x = 0; x < r_args->field_range; x++) {
+        for (int y = 0; y < r_args->field_range; y++) {
+            HPEN hPen = CreatePen(PS_SOLID, 2, r_args->colors[x][y]);
+            HBRUSH hBrush = CreateSolidBrush(r_args->colors[x][y]);
+
+            SelectObject(r_args->state->hdc, hPen);
+            SelectObject(r_args->state->hdc, hBrush);
+
+            Rectangle(r_args->state->hdc, r_args->state->width, r_args->state->height, 30, 30);
+        }
+    }
+
+    return 0;
+}
